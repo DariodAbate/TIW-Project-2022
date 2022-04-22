5ec459e57beb7d5a1642054e7c63b5df9c32a8f4
DariodAbate <dario.dabate@mail.polimi.it>
2022-04-22 11:35:09 +0200

    test commit

---
+++ src/main/java/it/polimi/tiw/test/ClassTEst1.java
@@ -0,0 +1,14 @@
+package it.polimi.tiw.test;
+
+public class ClassTEst1 {
+	String test;
+	
+	public ClassTEst1() {
+		this.test = "ciao";
+	}
+	
+	void print() {
+		System.out.println(test);
+	}
+	
+}
